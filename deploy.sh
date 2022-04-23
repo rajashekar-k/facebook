d109e6867bbcaddfcfb21e496f87d2f75faccd0a
rajashekar <kamerarajashekar@gmail.com>
2022-04-23 05:45:01 +0530

    updated master branch

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
-
-updating in master branch..
-this line updated in stage branch..
+updating in master branch...
 updated this file...
 again updated this file..
 this file contains the deployment script..
